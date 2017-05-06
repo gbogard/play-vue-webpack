e7c8ae925b72d1f8b811aed4657aa4fe3b2bea52
Guillaume Bogard <gouyou88@gmail.com>
2017-05-06 17:56:22 +0200

    Added a frontend.conf file with webpack options Renamed AppController to FrontController

--- project/WebpackServer.scala
+++ project/WebpackServer.scala
@@ -3,18 +3,22 @@ import java.net.InetSocketAddress
 
 import play.sbt.PlayRunHook
 import sbt._
+import com.typesafe.config._
 
 object WebpackServer {
   def apply(base: File): PlayRunHook = {
     object WebpackServerScript extends PlayRunHook {
       var process: Option[Process] = None
+      val config: Config = ConfigFactory.parseFile(new File("conf/frontend.conf")).resolve()
+      val isWin: Boolean = System.getProperty("os.name").toUpperCase().indexOf("WIN") >= 0
       override def afterStarted(add: InetSocketAddress): Unit = {
-        process = if (System.getProperty("os.name").toUpperCase().indexOf("WIN") >= 0)
-          Option(Process("cmd /c npm run watch", base).run)
+        val port = config.getInt("webpack.port")
+        val npmPath: String = if(!config.getIsNull("npm.path")) config.getString("npm.path") else "npm"
+        process = if (isWin)
+          Option(Process(s"cmd /c $npmPath run watch -- --port $port", base).run)
         else
-          Option(Process("npm run watch", base).run)
+          Option(Process(s"$npmPath run watch -- --port $port", base).run)
       }
-
       override def afterStopped(): Unit = {
         process.foreach(p => { p.destroy() })
         process = None
